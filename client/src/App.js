6e69274175ff4e175d3deba288bf662ac4dab55f
Joe Pfahl <joegravity@gmail.com>
2018-05-13 11:08:12 -0500

    Split job listing components out of dashboard

--- client/src/App.js
+++ client/src/App.js
@@ -5,8 +5,8 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-<Dashboard>
-  </Dashboard>
+      <Dashboard>
+      </Dashboard>
     );
   }
 }
--- client/src/Components/Dashboard/Dashboard.js
+++ client/src/Components/Dashboard/Dashboard.js
@@ -1,4 +1,6 @@
 import React from 'react';
+// import Joblist from "./Joblist";
+import { Joblist, Joblisting } from "../Joblist";
 import "./Dashboard.css";
 
 const Dashboard = props =>
@@ -15,20 +17,31 @@ const Dashboard = props =>
                     <button id="titlesort">Job Title</button>
                     <button id="employersort">Employer</button>
                 </div>
-                <div class="joblisting">
-                    <p class="title">Job Title  <span class="postdate">Jan 1</span></p>
-                    <p class="employer">Company Name <span class="location">City in MN</span></p>
-                    <p class="brief">Brief summary intro to job description, if available. Maybe first 1-3 lines of full description</p>
-            <button class="seejob">View Job Details</button>
-                    <select>
-                        <option value="apply">Apply</option>
-                        <option value="savejob" disabled>Save</option>
-                        <option value="dumpjob">Delete</option>
-                        <option value="audi">Audi</option>
-                    </select>
-                </div>
+                <Joblist>
+                    <Joblisting></Joblisting>
+                </Joblist>
+                <Joblist>
+                    <Joblisting></Joblisting>
+                </Joblist>
+                <Joblist>
+                    <Joblisting></Joblisting>
+                </Joblist>
             </div>
-            <a href="appliedjobs.html">Jobs Applied for </a> &nbsp;| &nbsp; <a href="#share_appliedjobs">Share</a>
+            <a href="#appliedjobs">Jobs Applied for </a> &nbsp;| &nbsp; <a href="#share_appliedjobs">Share</a>
+            <div id="appliedjobs">
+                <div id="sortlist">Sort by: &nbsp;
+            <button id="datesort">Date posted</button>
+                    <button id="prioritysort">Priority</button>
+                    <button id="titlesort">Job Title</button>
+                    <button id="employersort">Employer</button>
+                </div>
+                <Joblist>
+                    <Joblisting></Joblisting>
+                </Joblist>
+                <Joblist>
+                    <Joblisting></Joblisting>
+                </Joblist>
+            </div>
         </div >
     </div>
 
