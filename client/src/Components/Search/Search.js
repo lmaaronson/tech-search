0222a7b4a3b75d4bc8fe971bc2f2a8bacb8cb9ca
Dean Mamnev <deanmamnev@gmail.com>
2018-05-13 16:50:41 -0500

    skeleton placeholders in place, need functionality

--- client/src/Components/Search/Search.js
+++ client/src/Components/Search/Search.js
@@ -4,67 +4,68 @@ import "./Search.css";
 
 
 
-
-
-
 const Search = () => (
-<div>
-  <a className="active" href="#home">Home</a>
-  <a href="#about">About</a>
-  <a href="#contact">Contact</a>
-  <input type="text" id="myInput" onKeyUp="myFunction()" placeholder="Job Title"/>
-  <input type="text" id="myInput" onKeyUp="myFunction()" placeholder="Location"/>
-  
-
-<table id="myTable">
-  <tr className="header">
-    <th>Name</th>
-    <th>Country</th>
-  </tr>
-  <tr>
-    <td>Alfreds Futterkiste</td>
-    <td>Germany</td>
-  </tr>
-  <tr>
-    <td>Berglunds snabbkop</td>
-    <td>Sweden</td>
-  </tr>
-  <tr>
-    <td>Island Trading</td>
-    <td>UK</td>
-  </tr>
-  <tr>
-    <td>Koniglich Essen</td>
-    <td>Germany</td>
-  </tr>
-</table>
-
-
-
-</div>
-    );
-
-
-
-function myFunction() {
-  // Declare variables 
-  var input, filter, table, tr, td, i;
-  input = document.getElementById("myInput");
-  filter = input.value.toUpperCase();
-  table = document.getElementById("myTable");
-  tr = table.getElementsByTagName("tr");
-
-  // Loop through all table rows, and hide those who don't match the search query
-  for (i = 0; i < tr.length; i++) {
-    td = tr[i].getElementsByTagName("td")[0];
-    if (td) {
-      if (td.innerHTML.toUpperCase().indexOf(filter) > -1) {
-        tr[i].style.display = "";
-      } else {
-        tr[i].style.display = "none";
+    <div>
+      <a className="active" href="#home">Home</a>
+      <a href="#backend">BackEnd</a>
+      <a href="#frontend">FrontEnd</a>
+      <a href="#fullstack">Full Stack</a>
+      <a href="#profile">User Profile</a>
+     <p> 
+      <input type="text" id="myInput" onKeyUp="myFunction()" placeholder="Job Title"/>
+      <input type="text" id="myInput" onKeyUp="myFunction()" placeholder="Location"/>
+      </p>
+      
+    
+    <table id="myTable">
+      <tr className="header">
+        <th>Name</th>
+        <th>Country</th>
+      </tr>
+      <tr>
+        <td>Alfreds Futterkiste</td>
+        <td>Germany</td>
+      </tr>
+      <tr>
+        <td>Berglunds snabbkop</td>
+        <td>Sweden</td>
+      </tr>
+      <tr>
+        <td>Island Trading</td>
+        <td>UK</td>
+      </tr>
+      <tr>
+        <td>Koniglich Essen</td>
+        <td>Germany</td>
+      </tr>
+    </table>
+    
+    
+    
+    </div>
+        );
+    
+    
+    
+    function myFunction() {
+      // Declare variables 
+      var input, filter, table, tr, td, i;
+      input = document.getElementById("myInput");
+      filter = input.value.toUpperCase();
+      table = document.getElementById("myTable");
+      tr = table.getElementsByTagName("tr");
+    
+      // Loop through all table rows, and hide those who don't match the search query
+      for (i = 0; i < tr.length; i++) {
+        td = tr[i].getElementsByTagName("td")[0];
+        if (td) {
+          if (td.innerHTML.toUpperCase().indexOf(filter) > -1) {
+            tr[i].style.display = "";
+          } else {
+            tr[i].style.display = "none";
+          }
+        } 
       }
-    } 
-  }
-}
+    }
 
 export default Search;
