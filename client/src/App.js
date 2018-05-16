d8250f6325ccde8134c9f34f599a20f8dc1698a7
Michael <michael.m.scarr@gmail.com>
2018-05-16 11:23:53 -0500

    here we go

---
+++ client/src/App.js
@@ -0,0 +1,19 @@
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Search from "./pages/Search";
+//import Saved from "./pages/Saved";
+//import NoMatch from "./pages/NoMatch";
+import Nav from "./components/Nav";
+
+const App = () => (
+  <Router>
+    <div>
+      <Nav />
+      <Switch>
+        <Route exact path="/" component={Search} />
+      </Switch>
+    </div>
+  </Router>
+);
+
+export default App;
