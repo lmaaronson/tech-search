9b91c29222a4ccbe41bc31b5ac4e7fac883720cd
Dean Mamnev <deanmamnev@gmail.com>
2018-05-17 11:49:02 -0500

    Splash page created

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Search from "./pages/Search";
 import Saved from "./pages/Saved";
+import Splash from "./pages/Splash";
 //import Saved from "./pages/Saved";
 //import NoMatch from "./pages/NoMatch";
 import Nav from "./Components/Nav";
@@ -9,9 +10,8 @@ import Nav from "./Components/Nav";
 const App = () => (
   <Router>
     <div>
-      <Nav />
       <Switch>
-        <Route exact path="/" component={Saved} />
+        <Route exact path="/" component={Splash} />
       </Switch>
     </div>
   </Router>
